2a1b85e956ba6031f3f71fbf3134a651d05647f1
min_jjiny <kim003512@naver.com>
2020-04-20 23:33:51 +0900

    algo_10828(스택 관련 알고리즘)

--- algorithm_prac/src/src/algo_10828.java
+++ algorithm_prac/src/src/algo_10828.java
@@ -1,5 +1,27 @@
 package src;
 
-public class algo_10828 {
+import java.util.Scanner;
 
+public class algo_10828 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String order;
+		
+		order = sc.next();
+		
+		if(order == "push") {
+			
+		}else if(order == "pop") {
+			
+		}else if(order == "size") {
+			
+		}else if(order == "empty") {
+			
+		}else if(order == "top") {
+			
+		}else {
+			System.out.print("실행할 수 없는 명령어입니다.");
+		}
+	}
 }
