135a96c175f635d84d250b3081ee8f2c8b39e568
min_jjiny <kim003512@naver.com>
2020-04-22 19:06:23 +0900

    algo_10845(큐 클래스? 사용해서)

--- algorithm_prac/src/src/algo_10845.java
+++ algorithm_prac/src/src/algo_10845.java
@@ -1,61 +1,107 @@
 package src;
 
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class algo_10845 {
 	public static void main(String[] args) {
-		Scanner order_size = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
+		Queue<Integer> queue = new LinkedList<>();
 		
+		int order_size = sc.nextInt();
+		int value = 0; //why
+		String order;
+		
+		for(int i = 0; i < order_size; i++) {
+			order = sc.next();
+			
+			switch(order) {
+				case "push" :
+					value = sc.nextInt();
+					queue.add(value);
+					break;
+				case "pop" :
+					if(queue.isEmpty()) {
+						System.out.println(-1);
+					}else {
+						System.out.println(queue.poll());
+					}
+					break;
+				case "size" :
+					System.out.println(queue.size());
+					break;
+				case "empty" :
+					if(queue.isEmpty()) {
+						System.out.println(1);
+					}else {
+						System.out.println(0);
+					}
+					break;
+				case "front" :
+					if(queue.isEmpty()) {
+						System.out.println(-1);
+					}else {
+						System.out.println(queue.peek());
+					}
+				case "back" : 
+					if(queue.isEmpty()) {
+						System.out.println(-1);
+					}else {
+						System.out.println(value);
+					}
+					break;
+			}
+		}
 	}
 }
 
-class queue{
-	int front;
-	int rear;
-	int[] queue;
-	int size;
-	
-	//size: 큐에 들어있는 정수의 개수를 출력한다.
-	public queue(int size) {
-		front = 0;
-		rear = -1;
-		this.size= size;
-		queue = new int[size];
-		
-		System.out.println(size);
-	}
-	
-	//push X: 정수 X를 큐에 넣는 연산이다.
-	public void enQueue(int item) {
-		rear++;
-		queue[rear] = item;
-	}
-	
-	//pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 
-	//만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-	public int deQueue() {
-		if(front == rear + 1) {
-			//다시구현
-			return front;
-		}else {
-			return -1;
-		}
-	}
-	
-	//empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
-	public boolean isEmpty() {
-		if(front == rear + 1) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
-	//front: 큐의 가장 앞에 있는 정수를 출력한다. 
-	//만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-	
-	//back: 큐의 가장 뒤에 있는 정수를 출력한다. 
-	//만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-}
+//class queue{
+//	int front;
+//	int rear;
+//	int[] queue;
+//	int size;
+//	
+//	//size: 큐에 들어있는 정수의 개수를 출력한다.
+//	public queue(int size) {
+//		front = 0;
+//		rear = -1;
+//		this.size= size;
+//		queue = new int[size];
+//		
+//		System.out.println(size);
+//	}
+//	
+//	//push X: 정수 X를 큐에 넣는 연산이다.
+//	public void enQueue(int item) {
+//		rear++;
+//		queue[rear] = item;
+//	}
+//	
+//	//pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 
+//	//만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//	public int deQueue() {
+//		if(front == rear + 1) {
+//			//다시구현
+//			return front;
+//		}else {
+//			return -1;
+//		}
+//	}
+//	
+//	//empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
+//	public boolean isEmpty() {
+//		if(front == rear + 1) {
+//			return true;
+//		}else {
+//			return false;
+//		}
+//	}
+//	
+//	//front: 큐의 가장 앞에 있는 정수를 출력한다. 
+//	//만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//	
+//	//back: 큐의 가장 뒤에 있는 정수를 출력한다. 
+//	//만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//}
