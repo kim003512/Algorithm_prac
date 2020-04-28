5fe406debca48ec8e0bbde076ae332dd27c8fd42
min_jjiny <kim003512@naver.com>
2020-04-28 23:51:30 +0900

    algo_1149(조금)

--- algorithm_prac/src/src/algo_1149_dp.java
+++ algorithm_prac/src/src/algo_1149_dp.java
@@ -1,8 +1,22 @@
 package src;
 
+import java.util.Scanner;
+
 //https://www.acmicpc.net/problem/1149
 public class algo_1149_dp {
 	public static void main(String args[]) {
+//		int random = (int)(Math.random() * 1000) +2;
+//		
+//		System.out.println(random);
+		Scanner sc = new Scanner(System.in);
 		
+		int input_num = sc.nextInt();
+		
+		
+		for(int i = 0; i < input_num; i++) {
+			for(int j = 0; j < 3; j++) {
+				
+			}
+		}
 	}
 }
