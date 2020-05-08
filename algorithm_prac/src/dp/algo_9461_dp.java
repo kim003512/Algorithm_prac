990d7515694bf4ce73d6d0b3845bf493f65c452c
min_jjiny <kim003512@naver.com>
2020-05-08 22:41:50 +0900

    algo_9461(파도반 수열)

--- algorithm_prac/src/dp/algo_9461_dp.java
+++ algorithm_prac/src/dp/algo_9461_dp.java
@@ -1,11 +1,31 @@
 package dp;
 
+import java.io.*;
+import java.util.*;
+
 //https://www.acmicpc.net/problem/9461
 public class algo_9461_dp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		long[] P = new long[101];
+		P[1] = 1; P[2] = 1; P[3] = 1; P[4] = 2; P[5] = 2;
+		
+		for(int i = 6; i<= 100; i++) {
+			P[i] = P[i-1] + P[i -5];
+		}
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			int N = Integer.parseInt(br.readLine());
+			bw.write(P[N] + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
 	}
-
 }
