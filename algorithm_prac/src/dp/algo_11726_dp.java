974f4ad67c1a8a7eda5be0f6c0bdca4b47b1d9d1
min_jjiny <kim003512@naver.com>
2020-05-08 23:06:57 +0900

    algo_11726(2*n 타일)

--- algorithm_prac/src/dp/algo_11726_dp.java
+++ algorithm_prac/src/dp/algo_11726_dp.java
@@ -1,11 +1,27 @@
 package dp;
 
+import java.util.*;
+
 //https://www.acmicpc.net/problem/11726
 public class algo_11726_dp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int dp[] = new int[n+1];
+		
+		dp[1] = 1;
+		
+		//런타임 에러 방지??
+		if(n>=2) dp[2]=2;
+		
+		for(int i = 3; i<=n; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+			dp[i] = dp[i] % 1007;  //
+		}
+		
+		System.out.println(dp[n]);
 	}
 
 }
