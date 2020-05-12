3a480e617c6b03e8b562557fad9a6a71d751e78a
min_jjiny <kim003512@naver.com>
2020-05-12 22:05:11 +0900

    algo_1932(정수 삼각형)

--- algorithm_prac/src/dp/algo_1932_dp.java
+++ algorithm_prac/src/dp/algo_1932_dp.java
@@ -1,10 +1,23 @@
 package dp;
 
+import java.util.*;
+
 public class algo_1932_dp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[][] tri = new int[n][n];
+		
+		int num = 1;
+		
+		for(int i = 0; i<n; i++) {
+			for(int j=1; j <num; j++) {
+				
+			}
+		}
 	}
 
 }
