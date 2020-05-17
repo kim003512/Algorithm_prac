3cdf1806ba100244a8fa66f4497f66e633a24265
min_jjiny <kim003512@naver.com>
2020-05-17 14:14:45 +0900

    최소공배수 최대공약수 완료

--- algorithm_prac/src/least_greatest/algo_2609_least_greatest.java
+++ algorithm_prac/src/least_greatest/algo_2609_least_greatest.java
@@ -1,10 +1,31 @@
 package least_greatest;
 
+import java.util.*;
+
 public class algo_2609_least_greatest {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        Scanner sc = new Scanner(System.in);
+        
+        long num1 = sc.nextLong();
+        long num2 = sc.nextLong();
+        
+        long gcd = getGCD(Math.max(num1, num2), Math.min(num1, num2));
+        long lcm = getLCM(num1, num2, gcd);
+        
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+ 
+    public static long getGCD(long a, long b) {
+        while(b > 0) {
+            long tmp = a;
+            a = b;
+            b = tmp%b;
+        }
+        return a;
+    }
+    
+    public static long getLCM(long a, long b, long gcd) {
+        return (a*b)/gcd;
+    }
 }
