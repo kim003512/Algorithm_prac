e2909616ab73322bd11f7482e09f209e82daeb97
min_jjiny <kim003512@naver.com>
2020-05-17 13:38:37 +0900

    퇴사 완료

--- algorithm_prac/src/dp/algo_14501_dp.java
+++ algorithm_prac/src/dp/algo_14501_dp.java
@@ -1,9 +1,36 @@
 package dp;
 
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
 public class algo_14501_dp {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] T = new int[N + 2]; //1의 여유공간 추가 선언
+        int[] P = new int[N + 2];
+        int[] dp = new int[N + 2];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = N; i > 0; i--) {
+            int day = i + T[i];    
+
+            if (day <= N + 1)
+                dp[i] = Math.max(P[i] + dp[day], dp[i + 1]);
+            else   
+                dp[i] = dp[i + 1];
+        }
+
+        System.out.println(dp[1]);
 	}
 
 }
