2c3bb3e322bf1ad87025c29eee278344eaec8316
min_jjiny <kim003512@naver.com>
2020-05-17 13:58:46 +0900

    링 완료

--- algorithm_prac/src/least_greatest/algo_3036_least.java
+++ algorithm_prac/src/least_greatest/algo_3036_least.java
@@ -1,10 +1,35 @@
 package least_greatest;
 
+import java.io.*;
+import java.util.*;
+
 public class algo_3036_least {
+	public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] radius = new int[n];
+        for (int i = 0; i < n; i++) {
+            radius[i] = sc.nextInt();
+        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        for (int i = 1; i < n; i++) {
+            String answer = "";
+            int gcdNum = gcd(radius[i], radius[0]);
+            answer += radius[0] / gcdNum;
+            answer += "/";
+            answer += radius[i] / gcdNum;
 
-	}
+            System.out.println(answer);
+        }
+    }
 
+    public static int gcd(int first, int second) {
+        if (second <= 0) return first;
+
+        int temp = first;
+        first = second;
+        second = temp % second;
+
+        return gcd(first, second);
+    }
 }
