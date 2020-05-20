4c88a16743ef0e35ed589971e5a6d4906defab39
min_jjiny <kim003512@naver.com>
2020-05-20 20:33:37 +0900

    정렬(보물) 완료

--- algorithm_prac/src/array/algo_1026.java
+++ algorithm_prac/src/array/algo_1026.java
@@ -1,10 +1,38 @@
 package array;
 
+import java.util.*;
+
 public class algo_1026 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();		
+		int[] A = new int[N], B = new int[N];
+		int sum = 0;
 
+		
+		for(int i = 0; i<N ;i++) {
+			A[i] = sc.nextInt();
+		}
+		for(int i = 0; i < N; i++) {
+			B[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(A);
+		
+//		int bigB = B[0];
+//		
+//		for(int i = 0; i<B.length; i++) {
+//			if(bigB < B[i]) bigB=B[i];
+//		}
+		Arrays.sort(B);
+		
+		for(int i = 0; i<N; i++) {
+			sum += A[i] * B[N-1-i];
+		}
+		
+		System.out.println(sum);
 	}
 
 }
