19e661e14b0758277149cd47f9ef95221059339b
min_jjiny <kim003512@naver.com>
2020-05-26 22:37:47 +0900

    DFS와 BFS

--- algorithm_prac/src/graph/algo_1260.java
+++ algorithm_prac/src/graph/algo_1260.java
@@ -3,47 +3,68 @@ package graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 //https://www.acmicpc.net/problem/11724
 public class algo_1260 {
-
-	private static int visit[];
-    private static int [][]gra;
-    private static int cnt, N, M;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        visit = new int[N+1];
-        gra = new int[N+1][N+1];
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            gra[x][y] = gra[y][x] = 1;
-        }
-        cnt=0;
-        for (int i = 1; i <= N ; i++) { //i is a index of node to visit
-            if(visit[i] == 0){
-                cnt++;
-                dfs(i);
-            }
-        }
-        System.out.println(cnt);
-    }
-
-    private static void dfs(int node) {
-        visit[node] = cnt;
-
-        for (int i = 1; i <= N ; i++) {
-            if(gra[node][i] ==1 && visit[i] ==0)
-                dfs(i);
-        }
-    }
-
+	private static int[][] graph; // @1
+	private static boolean[] visit; // @2
+	private static int n, m, v; // @3
+	
+	public static void dfs(int v1) { // @4
+		visit[v1] = true;
+		System.out.print(v1 + " ");
+		
+		for(int j=1; j<=n; j++) {
+			if(graph[v1][j] == 1 && visit[j] == false) dfs(j);
+		}
+	}
+	
+	public static void bfs(int v1) { // @5
+		Queue<Integer> Q = new LinkedList<Integer>();
+		
+		Q.offer(v1);
+		visit[v1] = true;
+		System.out.print(v1 + " ");
+		
+		int tmp;
+		while(!Q.isEmpty()) {
+			tmp = Q.poll();
+			for(int j=1; j<n+1; j++) {
+				if(graph[tmp][j] == 1 && visit[j] == false) {
+					Q.offer(j);
+					visit[j] = true;
+					System.out.print(j + " ");
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] setInput = br.readLine().split(" ");
+		
+		n = Integer.parseInt(setInput[0]);
+		m = Integer.parseInt(setInput[1]);
+		v = Integer.parseInt(setInput[2]);
+		
+		
+		graph = new int[n+1][n+1];
+		visit = new boolean[n+1];
+		
+		while(m-- != 0) {
+			setInput = br.readLine().split(" ");
+			int x = Integer.parseInt(setInput[0]);
+			int y = Integer.parseInt(setInput[1]);
+			graph[x][y] = graph[y][x] = 1; // @6
+		}
+		
+		dfs(v);
+		for(int i=1; i<=n; i++) { // @7
+			visit[i] = false;
+		}
+		System.out.println();
+		bfs(v);
+		
+	}
 }
