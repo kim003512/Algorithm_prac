b0c96b94d3c25d355ba928bf8725e3ba6e769ff2
min_jjiny <kim003512@naver.com>
2020-06-01 16:38:39 +0900

    11724

--- algorithm_prac/src/graph/algo_11724.java
+++ algorithm_prac/src/graph/algo_11724.java
@@ -29,7 +29,7 @@ public class algo_11724 {
         for (int i = 1; i <= N ; i++) { //i = 방문하는 노드 인덱스
             if(visit[i] == 0){
                 cnt++;
-               // dfs(i);
+                dfs(i);
             }
         }
         System.out.println(cnt);
